6b8e56279a909eabc32dccece1da25fe7b0a67e0
shani gajera <gajerasunny15@gmail.com>
2024-03-01 11:20:00 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import './Header.css'
+
+
+const Header =() =>{
+return(
+    <>
+    <nav className="navbar navbar-expand-lg ">
+                <div className="container-fluid">
+                    <a className="navbar-brand" >Post</a>
+                </div>
+            </nav>
+    </>
+)
+}
+
+export default Header;
